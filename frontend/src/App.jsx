90b59322efb5d8dc00c994967662672b44318f5b
Krishna Pandit <krishnakumarpandit@krishnas-MacBook-Air.local>
2025-03-18 16:45:10 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import image1 from './assets/clothing.jpg';
+import assets from './assets/assets.js';
+
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-sky-200 text-center'>Hello World</h1>
+      <img src={assets.img_1_1} alt="logo" />
+    
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,11 @@
+import img1 from './clothing.jpg';
+import img2 from './logo_richlook.png';
+import img_1_1 from './img_1_1.png';
+
+const assets = {
+    img1,
+    img2,
+    img_1_1
+}
+
+export default assets;
