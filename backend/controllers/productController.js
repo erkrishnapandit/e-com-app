b885f1485d93ca79919541cc6437a89186faceb6
Krishna Pandit <krishnakumarji100@gmail.com>
2025-06-20 18:49:03 +0530

    List Product feature is added successfully

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -56,9 +56,13 @@ const listProduct = async (req, res)=>{
 
     try{
 
-        
+        const products = await productModel.find({});
+        res.json({success:true, products});
+
+
     }catch(error){
-        
+        console.log(error)
+        res.json({success:false, msg:'Internal Server Error'});
     }
 
 }
